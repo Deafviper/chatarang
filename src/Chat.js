e82fdd661d25f7a7cf93158fd46f77cd4d6156ff
Thomas Salas <tjsalas18@sbcglobal.net>
2018-07-03 15:23:59 -0400

    added message form

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import ChatHeader from './ChatHeader'
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
 class Chat extends Component {
   render() {
     return (
       <div className="Chat">
         <ChatHeader />
+        <MessageList/>
+        <MessageForm/>
         Chat
       </div>
     )
--- src/Main.js
+++ src/Main.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react'
 import Sidebar from './Sidebar'
 import Chat from './Chat'
 
+
 class Main extends Component {
   render() {
     return (
       <div className="Main" style={styles}>
         <Sidebar />
         <Chat />
+        
       </div>
     )
   }
