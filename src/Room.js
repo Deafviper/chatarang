c56cf5e9f9f3fd9a67d5faa3d3048e6a48423901
Thomas Salas <tjsalas18@sbcglobal.net>
2018-07-10 13:42:54 -0400

    room name changes when room link clicked

---
+++ src/Room.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { StyleSheet, css } from 'aphrodite'
+
+const Room = ({ roomName, setCurrentRoom }) => {
+const handleClick = ev => {
+  ev.preventDefault()
+  setCurrentRoom(roomName)
+}
+
+  return (
+    <li className={css(styles.item)} key={roomName}>
+      <a onClick ={handleClick} href="/" className={css(styles.link)}>
+        {roomName}
+      </a>
+    </li>
+  )
+}
+
+const styles = StyleSheet.create({
+  item: {
+    marginBottom: '0.5rem',
+  },
+
+  link: {
+    display: 'block',
+    color: 'whitesmoke',
+    textDecoration: 'none',
+
+    '::before': {
+      content: '"# "',
+    },
+
+    ':hover': {
+      backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    },
+  },
+})
+
+export default Room
